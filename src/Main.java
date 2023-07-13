de62c6833db8b6432f12a71d0b7792600fdf4b9b
Eke Prosper <prosperjoechinedum@gmail.com>
2023-07-13 15:01:00 +0100

    Implemented error handling within the codebase.

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,44 @@ public class Main {
         final byte PERCENT = 100;
         final byte MONTHS_IN_YEAR = 12;
 
+        int principal = 0;
+        float monthlyInterestRate = 0;
+        short numberOfMonths = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate > 0 && annualInterestRate <= 30) {
+                monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than or equals to 30.");
+        }
 
-        System.out.print("Period: ");
-        byte years = scanner.nextByte();
-        int numberOfMonths = years * MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfMonths = (short) (years * MONTHS_IN_YEAR);
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30.");
+        }
 
         double mortgage = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfMonths))
                 / (Math.pow(1 + monthlyInterestRate, numberOfMonths) - 1);
-        String mortgageCurrency = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageCurrency);
+
+        String mortgageResult = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageResult);
     }
 }
